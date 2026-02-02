e0ba2c8ce727c30dad4ee8d80d1da434375fb86d
Brahym-dev <brahym.ballouk@gmail.com>
2026-02-02 22:09:24 +0100

    PatientCreateDto and PatientMapper

---
+++ src/main/java/com/cabinetMed/cabinetMedical/DTOs/PatientCreateDto.java
@@ -0,0 +1,35 @@
+package com.cabinetMed.cabinetMedical.DTOs;
+
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import jakarta.validation.constraints.NotBlank;
+
+import java.util.Date;
+
+public record PatientCreateDto (
+    Long id,
+    @NotBlank(message = "Nom est obligatoire.")
+    String nom,
+    @NotBlank(message = "obligatoire.")
+    String prenom,
+    @NotBlank(message = "obligatoire.")
+    String adresse,
+    String telephone,
+    String email,
+    @Temporal(TemporalType.DATE)
+    Date dateNaissance,
+    String groupeSanguin,
+    String situationFamiliale,
+    String numeroAssurance,
+    int sexe,
+    String profession,
+    double solde,
+    @NotBlank(message = "obligatoire.")
+    String cnie,
+    String notes,
+    String antecedentFamiliaux,
+    String antecedentsChurirgicaux,
+    String antecedentsMedicaux,
+    String antecedentAutres
+
+){}
--- src/main/java/com/cabinetMed/cabinetMedical/mapper/PatientMapper.java
+++ src/main/java/com/cabinetMed/cabinetMedical/mapper/PatientMapper.java
@@ -1,9 +1,32 @@
 package com.cabinetMed.cabinetMedical.mapper;
 
+import com.cabinetMed.cabinetMedical.DTOs.PatientCreateDto;
 import com.cabinetMed.cabinetMedical.DTOs.PatientResponseShortDto;
 import com.cabinetMed.cabinetMedical.entities.Patient;
 
 public class PatientMapper {
+    public static Patient toEntity(PatientCreateDto request){
+        return Patient.builder()
+                .nom(request.nom())
+                .prenom(request.prenom())
+                .adresse(request.adresse())
+                .telephone(request.telephone())
+                .email(request.email())
+                .dateNaissance(request.dateNaissance())
+                .groupeSanguin(request.groupeSanguin())
+                .situationFamiliale(request.situationFamiliale())
+                .numeroAssurance(request.numeroAssurance())
+                .sexe(request.sexe())
+                .profession(request.profession())
+                .solde(request.solde())
+                .cnie(request.cnie())
+                .notes(request.notes())
+                .antecedentFamiliaux(request.antecedentFamiliaux())
+                .antecedentsChurirgicaux(request.antecedentsChurirgicaux())
+                .antecedentsMedicaux(request.antecedentsMedicaux())
+                .antecedentAutres(request.antecedentAutres())
+                .build();
+    }
 
     public static PatientResponseShortDto toResponseShort(Patient patient){
         return new PatientResponseShortDto(
